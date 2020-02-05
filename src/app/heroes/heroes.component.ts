cb1d23cf2da66c00a74f2376163aee4baff24515
fzabid05 <abidzora88@gmail.com>
2020-02-05 18:27:19 +0100

    first commit

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.scss']
+})
+export class HeroesComponent implements OnInit {
+hero :Hero = {
+  id:1,
+  name : "windstorm",
+}
+heroes: Hero[];
+
+selectedHero : Hero;
+onSelect(hero : Hero): void {
+  this.selectedHero = hero;
+}
+getHeroes(): void {
+  this.heroes = this.heroService.getHeroes();
+}
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit() {
+    this.getHeroes();
+  }
+
+}
